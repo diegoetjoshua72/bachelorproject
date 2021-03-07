215472056e1bb0dd2763ef853818a125425f43c7
joshua <joshua.caumont@gmail.com>
2021-03-07 06:26:03 +0100

    trying to make the wasm work

--- koweb/www/js/kocheck.js
+++ koweb/www/js/kocheck.js
@@ -1,24 +1,22 @@
-const rust = import('./pkg/koweb.js');
+const rust = import("../pkg/koweb");
 
-function get_cmds(){
-    var program_string = window.editor.getValue();
-    console.log(program_string);
-    console.log(typeof(program_string));
-    return program_string;
+function get_cmds() {
+  var program_string = window.editor.getValue();
+  console.log(program_string);
+  console.log(typeof program_string);
+  return program_string;
 }
 
 rust
-  .then(m => {
-
+  .then((m) => {
     m.greet();
 
-    document.getElementById("run").addEventListener("click", 
-    ()=>{
+    document.getElementById("run").addEventListener("click", () => {
       m.run_test(get_cmds());
     });
   })
   .catch(console.error);
 
-  //i want to get the cmds from the editor and turn that into an cmd iterator
-  //in rust
-  //see how i can access wasm stuff now 
+//i want to get the cmds from the editor and turn that into an cmd iterator
+//in rust
+//see how i can access wasm stuff now
