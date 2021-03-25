621139ca9c9d6ad4b60974b68be82978bc1cf6fa
kami <joshua.caumont@gmail.com>
2021-03-25 14:15:18 +0100

    getting rid of webpack etc

--- koweb/www/js/kocheck.js
+++ koweb/www/js/kocheck.js
@@ -4,10 +4,9 @@ import init, { greeting, run_test } from '../pkg/koweb.js';
 async function run() {
     await init();
     greeting();
-    run_test("brah");
+    document.getElementById("run").onclick = run_test("brah");
 }
 run();
-document.getElementById("run").onclick = run();
 
 //i want to get the cmds from the editor and turn that into an cmd iterator
 //in rust
