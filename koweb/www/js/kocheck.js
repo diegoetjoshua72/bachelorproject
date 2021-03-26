02e7dc6a9498bd31f5cff59ba846df87c9c1a9b4
kami <joshua.caumont@gmail.com>
2021-03-26 16:54:08 +0100

    getting the text from the editor and trying to parse that text appropriately

--- koweb/www/js/kocheck.js
+++ koweb/www/js/kocheck.js
@@ -1,4 +1,4 @@
-
+import editor from './editor';
 import init, { greeting, run_test } from '../pkg/koweb.js';
 async function run() {
     await init();
@@ -7,8 +7,3 @@ async function run() {
 
 var run_button = document.getElementById("run");
 run_button.onclick = run();
-
-
-//i want to get the cmds from the editor and turn that into an cmd iterator
-//in rust
-//see how i can access wasm stuff n
