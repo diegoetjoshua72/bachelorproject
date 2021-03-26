7e79954c78fc89de228a287a00bb099e2fca4356
kami <joshua.caumont@gmail.com>
2021-03-26 17:38:51 +0100

    getting the text from the editor and trying to parse that text appropriately

--- koweb/src/lib.rs
+++ koweb/src/lib.rs
@@ -32,6 +32,18 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 //     Ok(())
 // }
 
+
+pub fn set_panic_hook() {
+    // When the `console_error_panic_hook` feature is enabled, we can call the
+    // `set_panic_hook` function at least once during initialization, and then
+    // we will get better error messages if our code ever panics.
+    //
+    // For more details see
+    // https://github.com/rustwasm/console_error_panic_hook#readme
+    #[cfg(feature = "console_error_panic_hook")]
+    console_error_panic_hook::set_once();
+}
+
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen]
@@ -86,6 +98,7 @@ pub fn greeting() {
 
 #[wasm_bindgen]
 pub fn run_test(cmds_from_js: &str) -> Result<(), JsValue> {
+    set_panic_hook();
     alert(cmds_from_js);
 
     // let opt = Opt{};
