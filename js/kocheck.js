46310f96e8703788d8db929fa8c32fc33dfb370f
diegoetjoshua72 <diegoetjoshua72@users.noreply.github.com>
2021-03-26 15:45:23 +0000

    deploy: af88aa311f825503cfdab69556be7892aa04c0bb

--- js/kocheck.js
+++ js/kocheck.js
@@ -2,13 +2,11 @@
 import init, { greeting, run_test } from '../pkg/koweb.js';
 async function run() {
     await init();
-    greeting();
-    var value = editor.getValue();
-    console.log(value);
-    var run = document.getElementById("run");
-    run.onclick = run_test(value);
+    run_test(editor.getValue());
 }
-run();
+
+var run = document.getElementById("run");
+run.onclick = run();
 
 
 //i want to get the cmds from the editor and turn that into an cmd iterator
