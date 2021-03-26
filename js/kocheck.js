d32ac7cc5a37670c4925b02bfd0effd78a5fe974
diegoetjoshua72 <diegoetjoshua72@users.noreply.github.com>
2021-03-26 14:47:30 +0000

    deploy: 36805e32ce1af67ee52ae19c17a1be550721dc79

--- js/kocheck.js
+++ js/kocheck.js
@@ -1,10 +1,10 @@
 
-
+import editor from './editor.js'
 import init, { greeting, run_test } from '../pkg/koweb.js';
 async function run() {
     await init();
     greeting();
-    var value = window.editor.getValue();
+    var value = editor.getValue();
     console.log(value);
     var run = document.getElementById("run");
     run.onclick = run_test(value);
@@ -12,7 +12,6 @@ async function run() {
 }
 run();
 
-greeting();
 
 //i want to get the cmds from the editor and turn that into an cmd iterator
 //in rust
