9e7f13c35819d222b2ea158529ccdeb788e77cc5
diegoetjoshua72 <diegoetjoshua72@users.noreply.github.com>
2021-03-28 05:36:44 +0000

    deploy: daa34506c44d3c95f99f6862e1d1580e07f833a4

--- js/kocheck.js
+++ js/kocheck.js
@@ -3,7 +3,7 @@ import {editor,init_editor,hello} from './editor.js';
 import init, { greeting, run_test } from '../pkg/koweb.js';
 // import * as wasm from '../pkg/koweb.js';
 
-await init_editor();
+init_editor();
 
 async function run() {
     await init();
