f564b92eb859b98a76703c7068eec9da3e827b5b
kami <joshua.caumont@gmail.com>
2021-03-28 07:41:02 +0200

    getting the test un rust

--- koweb/www/js/kocheck.js
+++ koweb/www/js/kocheck.js
@@ -1,16 +1,16 @@
 
-import {init_editor,hello} from './editor.js';
+import {init_editor} from './editor.js';
 import init, { greeting, run_test } from '../pkg/koweb.js';
 // import * as wasm from '../pkg/koweb.js';
 
 var editor = init_editor();
+console.log(editor);
 
 async function run() {
     await init();
     run_test(editor.getValue());
 }
 
-hello();
 
 var run_button = document.getElementById("run");
 run_button.addEventListener('click', run());
