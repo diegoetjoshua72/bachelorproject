8f19b24ee8f28d57212f1a7ccc833a316125a18c
kami <joshua.caumont@gmail.com>
2021-03-31 15:38:05 +0200

    making the real thing work and loging

--- koweb/src/lib.rs
+++ koweb/src/lib.rs
@@ -64,7 +64,6 @@ impl JsOpt {
 
 fn produce_from_js(cmds_from_js: &'static str ,opt: &Opt ) -> impl Iterator<Item = Result<Event, Error>>{
     let cmds = parse(cmds_from_js.as_bytes(), opt).map(|cmd| cmd.map(Event::Command));
-    //what is the return of parse really is something i need to understand here !!
     cmds 
 }
 
@@ -140,48 +139,48 @@ pub fn run_test(cmds_from_js: String, eta: bool, no_scope: bool, no_infer: bool 
     //calling parse on my string when passing it in parse .as_bytes()
     // https://stackoverflow.com/questions/32674905/pass-string-to-function-taking-read-trait
 
-    let cmds = [
-        // declarations
-        "prop :.\n",
-        "imp : prop -> prop -> prop.\n",
-        // definition with a rewrite rule
-        "def proof : prop -> Type.\n",
-        "[x, y] proof (imp x y) --> proof x -> proof y.\n",
-        // theorem
-        r"thm imp_refl (x : prop) : proof (imp x x) := p : proof x => p.\n",
-    ];
+    // let cmds = [
+    //     // declarations
+    //     "prop :.\n",
+    //     "imp : prop -> prop -> prop.\n",
+    //     // definition with a rewrite rule
+    //     "def proof : prop -> Type.\n",
+    //     "[x, y] proof (imp x y) --> proof x -> proof y.\n",
+    //     // theorem
+    //     r"thm imp_refl (x : prop) : proof (imp x x) := p : proof x => p.\n",
+    // ];
 
-    let arena = Arena::new();
-    let mut syms = Symbols::new();
-    let mut sig = Signature::new();
+    // let arena = Arena::new();
+    // let mut syms = Symbols::new();
+    // let mut sig = Signature::new();
 
-    for c in cmds.iter() {
-        // parse and scope command in one go
-        let cmd: Command<String> = Command::parse(c, &syms).unwrap();
-        match cmd {
-            // introduction of a new name
-            Command::Intro(id, it) => {
-                let id: &str = arena.alloc(id);
-                // add symbol to symbol table and fail if it is not new
-                let sym = syms.insert(id).unwrap();
+    // for c in cmds.iter() {
+    //     // parse and scope command in one go
+    //     let cmd: Command<String> = Command::parse(c, &syms).unwrap();
+    //     match cmd {
+    //         // introduction of a new name
+    //         Command::Intro(id, it) => {
+    //             let id: &str = arena.alloc(id);
+    //             // add symbol to symbol table and fail if it is not new
+    //             let sym = syms.insert(id).unwrap();
 
-                //TODO
-                //need to figure out how i can do error handling code and pass them to the js
-                // match sym {
-                //     Ok(symbol) => sym = symbol,
-                //     Err(SymbolsError) => return Ok(JsValue.from_str("test")),
-                // }
+    //             //TODO
+    //             //need to figure out how i can do error handling code and pass them to the js
+    //             // match sym {
+    //             //     Ok(symbol) => sym = symbol,
+    //             //     Err(SymbolsError) => return Ok(JsValue.from_str("test")),
+    //             // }
 
-                // typecheck and insert into signature
-                let typing: Typing = Typing::new(Intro::from(it), &sig)
-                    .unwrap()
-                    .check(&sig)
-                    .unwrap();
-                sig.insert(sym, typing).unwrap()
-            }
-            // addition of rewrite rules
-            Command::Rules(rules) => sig.add_rules(rules.into_iter().map(Rule::from)).unwrap(),
-        }
-    }
+    //             // typecheck and insert into signature
+    //             let typing: Typing = Typing::new(Intro::from(it), &sig)
+    //                 .unwrap()
+    //                 .check(&sig)
+    //                 .unwrap();
+    //             sig.insert(sym, typing).unwrap()
+    //         }
+    //         // addition of rewrite rules
+    //         Command::Rules(rules) => sig.add_rules(rules.into_iter().map(Rule::from)).unwrap(),
+    //     }
+    // }
     Ok(())
 }
