ed645f1167aeb1c0a8bf55d38c0ecf078ecfad38
kami <joshua.caumont@gmail.com>
2021-04-01 17:24:10 +0200

    trying to get good debug messages rather than unreachable and make log work

--- koweb/src/lib.rs
+++ koweb/src/lib.rs
@@ -11,7 +11,8 @@ use kontroli::error::SymbolsError;
 
 use byte_unit::{Byte, ByteError};
 use kocheck::{Opt, Event, parse, seq, Error};
-use log::{info, trace, warn};
+
+use log::{info, trace, warn, Level};
 
 //not sure what wee alloc does
 #[cfg(feature = "wee_alloc")]
@@ -98,7 +99,8 @@ where I : Iterator<Item = Result<Event, Error>>,
 #[wasm_bindgen]
 pub fn run_test(cmds_from_js: String, eta: bool, no_scope: bool, no_infer: bool , no_check: bool) -> Result<(), JsValue> {
 
-    wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
+    // wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
+    console_log::init_with_level(Level::Trace);
     init_console_wasm_debug();
     alert(cmds_from_js.as_str());
 
