d5e9a367de844a37f19bc97a4a2a5eb82ec6523d
joshua <joshua.caumont@gmail.com>
2021-04-12 16:50:07 +0200

    fixing things

--- koweb/src/lib.rs
+++ koweb/src/lib.rs
@@ -10,7 +10,7 @@ use kontroli::scope::{Command, Symbols};
 use kontroli::error::SymbolsError;
 
 use byte_unit::{Byte, ByteError};
-use kocheck::{Opt, Event, parse, seq, Error};
+use kocheck::{parse, seq, Error, Event, Opt};
 
 use log::{info, trace, warn, Level};
 
@@ -46,8 +46,11 @@ pub fn greeting() {
     alert("debug code is run");
 }
 
-fn produce_from_js(cmds_from_js: &'static str ,opt: &Opt ) -> impl Iterator<Item = Result<Event, Error>>{
-    let module = std::iter::once(Ok(Event::Module(vec!("js".to_string()))));
+fn produce_from_js(
+    cmds_from_js: &'static str,
+    opt: &Opt,
+) -> impl Iterator<Item = Result<Event, Error>> {
+    let module = std::iter::once(Ok(Event::Module(vec!["js".to_string()])));
     let cmds = parse(cmds_from_js.as_bytes(), opt).map(|cmd| cmd.map(Event::Command));
     module.chain(cmds)
 }
@@ -56,7 +59,7 @@ fn string_to_static_str(s: String) -> &'static str {
     Box::leak(s.into_boxed_str())
 }
 
-fn write_to_webconsole(event: &kocheck::Event){
+fn write_to_webconsole(event: &kocheck::Event) {
     //i need to match here on the cocheck enum ??
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
@@ -64,11 +67,11 @@ fn write_to_webconsole(event: &kocheck::Event){
 
     // let val = document.get_element_by_id("console").unwrap();
     let val = document.create_element("p").unwrap();
-    val.set_text_content(Some(format!("{}",event).as_str()));
+    val.set_text_content(Some(format!("{}", event).as_str()));
     body.append_child(&val).unwrap();
 }
 
-//temporary i'll use the definition from bin maybe 
+//temporary i'll use the definition from bin maybe
 pub fn flatten_nested_results<O, I, T, E>(outer: O) -> impl Iterator<Item = Result<T, E>>
 where
     O: Iterator<Item = Result<I, E>>,
@@ -83,22 +86,42 @@ where
     })
 }
 
-
-fn print_iterator<I>(iter: &mut I )
-where I : Iterator<Item = Result<Event, Error>>,
+fn print_iterator<I>(iter: &mut I)
+where
+    I: Iterator<Item = Result<Event, Error>>,
 {
     for element in iter {
-        match element{
-            Result::Ok(Event) => log(format!("{}",Event).as_str()),
-            
-            Result::Err(Error) => log(format!("something went wrong : {:?}",Error).as_str())
+        match element {
+            Result::Ok(Event) => log(format!("{}", Event).as_str()),
+
+            Result::Err(Error) => log(format!("something went wrong : {:?}", Error).as_str()),
         }
     }
 }
 
-#[wasm_bindgen]
-pub fn run_test(cmds_from_js: String, eta: bool, no_scope: bool, no_infer: bool , no_check: bool) -> Result<(), JsValue> {
+static mut test: i32 = 0;
 
+#[wasm_bindgen()]
+pub fn increment() {
+    unsafe {
+        alert(format!("test : {}", test));
+        test += 1;
+    }
+}
+
+pub fn lazy() {
+    // goal is to get only a piece of the string if there is nothing left to get
+    // then it returns what ? False
+}
+
+#[wasm_bindgen]
+pub fn run_test(
+    cmds_from_js: String,
+    eta: bool,
+    no_scope: bool,
+    no_infer: bool,
+    no_check: bool,
+) -> Result<(), JsValue> {
     // wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
     console_log::init_with_level(Level::Trace);
     init_console_wasm_debug();
@@ -108,51 +131,45 @@ pub fn run_test(cmds_from_js: String, eta: bool, no_scope: bool, no_infer: bool 
     // https://stackoverflow.com/questions/19846078/how-to-read-from-chromes-console-in-javascript
     //CAREFULLLLL when something goes wrong in the code i get unreachable in the browser console i need to find a way to get good error messages
     //essayer de virer le static lifetime =)
-    //essayer le wasm log 
     //essayer de passer de maniere async le code dans le text editor (lazy)
     //verification de fichier passer par le file system ou url
-    //run from : ---- 
-        //error
-        // -> String ??? -> (peut etre async lazy reading)???
-        //regarder le parse buffer
+    //run from : ----
+    //error
+    // -> String ??? -> (peut etre async lazy reading)???
+    //regarder le parse buffer
 
-    //typing reduce convertible 
+    //typing reduce convertible
     //something i wrong and i reach unreachable how can i fix that the logging works tho which is really cool
-
 
     // env_logger::from_env(Env::default().filter_or("LOG", "warn")).init();
     // log::debug!("hello");
-    
     // Introduce symbol {|Pure.Appt|const|}
 
-    let static_cmds_str  = string_to_static_str(cmds_from_js);
+    let static_cmds_str = string_to_static_str(cmds_from_js);
 
-    
     let opt = Opt {
         eta,
         no_scope,
         no_infer,
         no_check,
-        buffer : Byte::from_str("64MB").unwrap(),
-        channel_capacity : None ,
-        jobs : None,
-        files : vec!(),
+        buffer: Byte::from_str("64MB").unwrap(),
+        channel_capacity: None,
+        jobs: None,
+        files: vec![],
     };
 
     let iter = produce_from_js(static_cmds_str, &opt);
-    
-    let mut iter = Box::new(iter).inspect(|r| r.iter().for_each(|event| write_to_webconsole(event)));
-    
+
+    let mut iter =
+        Box::new(iter).inspect(|r| r.iter().for_each(|event| write_to_webconsole(event)));
     // alert(format!("{}",iter.size_hint()));
-    //lets print out the iterator to the console i think that would be useful information 
+    //lets print out the iterator to the console i think that would be useful information
     // print_iterator(&mut iter);
-    
 
     seq::consume(iter, &opt).expect("something went wrong in the consume");
 
-    
-    //i want to be able to get error messages and line numbers 
-    //then i want to be able to run it with the run button 
+    //i want to be able to get error messages and line numbers
+    //then i want to be able to run it with the run button
     //make the loading bar thing
     //calling parse on my string when passing it in parse .as_bytes()
     // https://stackoverflow.com/questions/32674905/pass-string-to-function-taking-read-trait
