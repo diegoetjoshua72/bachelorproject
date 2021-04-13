20e210477c934b76a6d2c9cfc7ba514d59bc8283
diegoetjoshua72 <diegoetjoshua72@users.noreply.github.com>
2021-04-13 08:04:04 +0000

    deploy: e9bf8ff5610f244ef162dcd9b5a77e0a5c0b4677

--- js/editor.js
+++ js/editor.js
@@ -1,7 +1,4 @@
-
-
 // import * as loader from '../monaco/min/vs/loader.js'
-
 
 // export function init_editor(){
 
@@ -9,15 +6,15 @@
 //   define('main', ["./monaco/min/vs/editor/editor.main"], function () {
 //     console.log("inside");
 //     window.test = "hello from inside"
-    
+
 //     return {someValue: 2};
-  
+
 //     });
 
 //   console.log(window.test);
 
-
-{/* <script>
+{
+    /* <script>
 var require = { paths: { vs: '../node_modules/monaco-editor/min/vs' } };
 </script>
 <script src="../node_modules/monaco-editor/min/vs/loader.js"></script>
@@ -29,305 +26,303 @@ var editor = monaco.editor.create(document.getElementById('container'), {
     value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
     language: 'javascript'
 });
-</script> */}
-
-
+</script> */
+}
 
 var editor = 0;
 
-  require.config({ paths: { vs: "./monaco/min/vs" } });
-  require(["vs/editor/editor.main"], function () {
-  
+require.config({ paths: { vs: "./monaco/min/vs" } });
+require(["vs/editor/editor.main"], function () {
     monaco.languages.register({
-      id: "lambdapi",
+        id: "lambdapi",
     });
 
-    
-
     monaco.languages.setMonarchTokensProvider("lambdapi", {
-      tokenizer: {
-        root: [
-          [/\b(as|begin|builtin|constant|debug|end|flag|in|infix|injective|left|off|notation|on|opaque|prover|prover_timeout|prefix|private|protected|quantifier|right|sequential|TYPE|unif_rule)\b/,"keyword"],
-        ],
-      },
+        tokenizer: {
+            root: [
+                [
+                    /\b(as|begin|builtin|constant|debug|end|flag|in|infix|injective|left|off|notation|on|opaque|prover|prover_timeout|prefix|private|protected|quantifier|right|sequential|TYPE|unif_rule)\b/,
+                    "keyword",
+                ],
+            ],
+        },
     });
 
     //   // Define a new theme that constains only rules that match this language
     monaco.editor.defineTheme("vs-gruv", {
-      base: "vs-dark",
-      inherit: true,
-      rules: [
-        {
-          foreground: "908476",
-          token: "comment",
+        base: "vs-dark",
+        inherit: true,
+        rules: [
+            {
+                foreground: "908476",
+                token: "comment",
+            },
+            {
+                foreground: "aab11e",
+                token: "string",
+            },
+            {
+                foreground: "cf8498",
+                token: "punctuation.separator.key-value",
+            },
+            {
+                foreground: "cc869b",
+                token: "constant",
+            },
+            {
+                foreground: "ead4af",
+                token: "variable",
+            },
+            {
+                foreground: "cab990",
+                token: "variable.other.object",
+            },
+            {
+                foreground: "f1c050",
+                token: "variable.other.class",
+            },
+            {
+                foreground: "f1c050",
+                token: "variable.other.constant",
+            },
+            {
+                foreground: "ead4af",
+                token: "meta.property.object",
+            },
+            {
+                foreground: "ead4af",
+                token: "entity.name.tag",
+            },
+            {
+                foreground: "ead4af",
+                token: "meta.function",
+            },
+            {
+                foreground: "ead4af",
+                token: "meta.function.static.arrow",
+            },
+            {
+                foreground: "ead4af",
+                token: "meta.function.arrow",
+            },
+            {
+                foreground: "fb4938",
+                token: "keyword",
+            },
+            {
+                foreground: "fb4938",
+                token: "string.regexp punctuation.definition",
+            },
+            {
+                foreground: "fb4938",
+                token: "storage",
+            },
+            {
+                foreground: "fb4938",
+                token: "storage.type",
+            },
+            {
+                foreground: "fb4938",
+                token: "markup.underline.link",
+            },
+            {
+                foreground: "babc52",
+                token: "entity.name.class",
+            },
+            {
+                foreground: "babc52",
+                token: "entity.name.type.class",
+            },
+            {
+                foreground: "7ba093",
+                token: "entity.other.inherited-class",
+            },
+            {
+                foreground: "7ba093",
+                token: "tag.decorator",
+            },
+            {
+                foreground: "7ba093",
+                token: "tag.decorator entity.name.tag",
+            },
+            {
+                foreground: "8ab572",
+                token: "entity.name.function",
+            },
+            {
+                foreground: "8ab572",
+                token: "meta.function entity.name.function",
+            },
+            {
+                foreground: "fd971f",
+                token: "variable.parameter",
+            },
+            {
+                foreground: "fd971f",
+                token: "meta.function storage.type",
+            },
+            {
+                foreground: "fb4938",
+                fontStyle: " italic  ",
+                token: "entity.name.tag",
+            },
+            {
+                foreground: "8ab572",
+                fontStyle: " italic",
+                token: "entity.other.attribute-name",
+            },
+            {
+                foreground: "f1c050",
+                token: "support.type",
+            },
+            {
+                foreground: "f1c050",
+                token: "support.class",
+            },
+            {
+                foreground: "f1c050",
+                token: "support.function",
+            },
+            {
+                foreground: "f1c050",
+                token: "variable.language",
+            },
+            {
+                foreground: "f1c050",
+                token: "support.constant",
+            },
+            {
+                foreground: "f1c050",
+                token: "string.regexp keyword.control",
+            },
+            {
+                foreground: "8ab572",
+                token: "punctuation.template-string.element",
+            },
+            {
+                foreground: "8ab572",
+                token: "string.regexp punctuation.definition.group",
+            },
+            {
+                foreground: "8ab572",
+                token: "constant.character.escape",
+            },
+            {
+                foreground: "f8f8f0",
+                background: "fb4938",
+                token: "invalid",
+            },
+            {
+                foreground: "f8f8f0",
+                background: "fd971f",
+                token: "invalid.deprecated",
+            },
+            {
+                foreground: "cab990",
+                token: "keyword.operator",
+            },
+            {
+                foreground: "cab990",
+                token: "keyword.operator.logical",
+            },
+            {
+                foreground: "cab990",
+                token: "meta.property-name",
+            },
+            {
+                foreground: "cab990",
+                token: "meta.brace",
+            },
+            {
+                foreground: "cab990",
+                token: "punctuation.definition.parameters.begin",
+            },
+            {
+                foreground: "cab990",
+                token: "punctuation.definition.parameters.end",
+            },
+            {
+                foreground: "cab990",
+                token: "keyword.other.parenthesis",
+            },
+            {
+                foreground: "7ba093",
+                token: "keyword.operator.ternary",
+            },
+            {
+                foreground: "ead4af",
+                token: "punctuation.separator.parameter",
+            },
+            {
+                foreground: "fb4938",
+                token: "keyword.operator.module",
+            },
+            {
+                foreground: "d02000",
+                token: "sublimelinter.mark.error",
+            },
+            {
+                foreground: "ddb700",
+                token: "sublimelinter.mark.warning",
+            },
+            {
+                foreground: "ffffff",
+                token: "sublimelinter.gutter-mark",
+            },
+            {
+                foreground: "70c060",
+                token: "markup.inserted",
+            },
+            {
+                foreground: "ddb700",
+                token: "markup.changed",
+            },
+            {
+                foreground: "fb4938",
+                token: "markup.deleted",
+            },
+        ],
+        colors: {
+            "editor.foreground": "#EAD4AF",
+            "editor.background": "#282828",
+            "editor.selectionBackground": "#3B3836",
+            "editor.lineHighlightBackground": "#3B3836",
+            "editorCursor.foreground": "#908476",
+            "editorWhitespace.foreground": "#3B3836",
         },
-        {
-          foreground: "aab11e",
-          token: "string",
-        },
-        {
-          foreground: "cf8498",
-          token: "punctuation.separator.key-value",
-        },
-        {
-          foreground: "cc869b",
-          token: "constant",
-        },
-        {
-          foreground: "ead4af",
-          token: "variable",
-        },
-        {
-          foreground: "cab990",
-          token: "variable.other.object",
-        },
-        {
-          foreground: "f1c050",
-          token: "variable.other.class",
-        },
-        {
-          foreground: "f1c050",
-          token: "variable.other.constant",
-        },
-        {
-          foreground: "ead4af",
-          token: "meta.property.object",
-        },
-        {
-          foreground: "ead4af",
-          token: "entity.name.tag",
-        },
-        {
-          foreground: "ead4af",
-          token: "meta.function",
-        },
-        {
-          foreground: "ead4af",
-          token: "meta.function.static.arrow",
-        },
-        {
-          foreground: "ead4af",
-          token: "meta.function.arrow",
-        },
-        {
-          foreground: "fb4938",
-          token: "keyword",
-        },
-        {
-          foreground: "fb4938",
-          token: "string.regexp punctuation.definition",
-        },
-        {
-          foreground: "fb4938",
-          token: "storage",
-        },
-        {
-          foreground: "fb4938",
-          token: "storage.type",
-        },
-        {
-          foreground: "fb4938",
-          token: "markup.underline.link",
-        },
-        {
-          foreground: "babc52",
-          token: "entity.name.class",
-        },
-        {
-          foreground: "babc52",
-          token: "entity.name.type.class",
-        },
-        {
-          foreground: "7ba093",
-          token: "entity.other.inherited-class",
-        },
-        {
-          foreground: "7ba093",
-          token: "tag.decorator",
-        },
-        {
-          foreground: "7ba093",
-          token: "tag.decorator entity.name.tag",
-        },
-        {
-          foreground: "8ab572",
-          token: "entity.name.function",
-        },
-        {
-          foreground: "8ab572",
-          token: "meta.function entity.name.function",
-        },
-        {
-          foreground: "fd971f",
-          token: "variable.parameter",
-        },
-        {
-          foreground: "fd971f",
-          token: "meta.function storage.type",
-        },
-        {
-          foreground: "fb4938",
-          fontStyle: " italic  ",
-          token: "entity.name.tag",
-        },
-        {
-          foreground: "8ab572",
-          fontStyle: " italic",
-          token: "entity.other.attribute-name",
-        },
-        {
-          foreground: "f1c050",
-          token: "support.type",
-        },
-        {
-          foreground: "f1c050",
-          token: "support.class",
-        },
-        {
-          foreground: "f1c050",
-          token: "support.function",
-        },
-        {
-          foreground: "f1c050",
-          token: "variable.language",
-        },
-        {
-          foreground: "f1c050",
-          token: "support.constant",
-        },
-        {
-          foreground: "f1c050",
-          token: "string.regexp keyword.control",
-        },
-        {
-          foreground: "8ab572",
-          token: "punctuation.template-string.element",
-        },
-        {
-          foreground: "8ab572",
-          token: "string.regexp punctuation.definition.group",
-        },
-        {
-          foreground: "8ab572",
-          token: "constant.character.escape",
-        },
-        {
-          foreground: "f8f8f0",
-          background: "fb4938",
-          token: "invalid",
-        },
-        {
-          foreground: "f8f8f0",
-          background: "fd971f",
-          token: "invalid.deprecated",
-        },
-        {
-          foreground: "cab990",
-          token: "keyword.operator",
-        },
-        {
-          foreground: "cab990",
-          token: "keyword.operator.logical",
-        },
-        {
-          foreground: "cab990",
-          token: "meta.property-name",
-        },
-        {
-          foreground: "cab990",
-          token: "meta.brace",
-        },
-        {
-          foreground: "cab990",
-          token: "punctuation.definition.parameters.begin",
-        },
-        {
-          foreground: "cab990",
-          token: "punctuation.definition.parameters.end",
-        },
-        {
-          foreground: "cab990",
-          token: "keyword.other.parenthesis",
-        },
-        {
-          foreground: "7ba093",
-          token: "keyword.operator.ternary",
-        },
-        {
-          foreground: "ead4af",
-          token: "punctuation.separator.parameter",
-        },
-        {
-          foreground: "fb4938",
-          token: "keyword.operator.module",
-        },
-        {
-          foreground: "d02000",
-          token: "sublimelinter.mark.error",
-        },
-        {
-          foreground: "ddb700",
-          token: "sublimelinter.mark.warning",
-        },
-        {
-          foreground: "ffffff",
-          token: "sublimelinter.gutter-mark",
-        },
-        {
-          foreground: "70c060",
-          token: "markup.inserted",
-        },
-        {
-          foreground: "ddb700",
-          token: "markup.changed",
-        },
-        {
-          foreground: "fb4938",
-          token: "markup.deleted",
-        },
-      ],
-      colors: {
-        "editor.foreground": "#EAD4AF",
-        "editor.background": "#282828",
-        "editor.selectionBackground": "#3B3836",
-        "editor.lineHighlightBackground": "#3B3836",
-        "editorCursor.foreground": "#908476",
-        "editorWhitespace.foreground": "#3B3836",
-      },
     });
 
     //this is taken outside the normal execution flow and its a problem because i need window to be set before i continue
     window.editor = monaco.editor.create(document.getElementById("container"), {
-      theme: "vs-gruv",
-      value: getCode(),
-      language: "lambdapi",
+        theme: "vs-gruv",
+        value: getCode(),
+        language: "lambdapi",
+        automaticLayout: true,
     });
-  });                                                                                                                                                                                                                   ;
+});
 
-
-function getCode () {
-  return [
-    "(; Adding additional arguments via rewriting, by Gaspard Ferey. ;)",
-    "N : Type.",
-    "0 : N.",
-    "S : N -> N.",
-    "A : Type.",
-    "List : N -> Type.",
-    "def magic : N -> N -> Type.",
-    "[n  ] magic n 0      -->  List n",
-    "[n,i] magic n (S i)  -->  A -> magic n i.",
-    "cons : n : N -> magic n n.",
-    "def checks",
-    "  : A -> A -> A -> List (S (S (S 0)))",
-    "  := cons (S (S (S 0))).",
-    "(;",
-    "def fails",
-    "  : A -> A -> A -> List (S (S (S 0)))",
-    "  := cons (S (S (0))).",
-    ";)",
-  ].join("\n")
+function getCode() {
+    return [
+        "(; Adding additional arguments via rewriting, by Gaspard Ferey. ;)",
+        "N : Type.",
+        "0 : N.",
+        "S : N -> N.",
+        "A : Type.",
+        "List : N -> Type.",
+        "def magic : N -> N -> Type.",
+        "[n  ] magic n 0      -->  List n",
+        "[n,i] magic n (S i)  -->  A -> magic n i.",
+        "cons : n : N -> magic n n.",
+        "def checks",
+        "  : A -> A -> A -> List (S (S (S 0)))",
+        "  := cons (S (S (S 0))).",
+        "(;",
+        "def fails",
+        "  : A -> A -> A -> List (S (S (S 0)))",
+        "  := cons (S (S (0))).",
+        ";)",
+    ].join("\n");
 }
-
 
 // function getCode() {
 //   return [
@@ -423,6 +418,3 @@ function getCode () {
 //     "thm {|Pure.conjunction_imp|thm|} : A : eta {|prop|type|} -> B : eta {|prop|type|} -> C : eta {|prop|type|} -> eps ({|Pure.eq|const|} {|prop|type|} ({|Pure.imp|const|} ({|Pure.conjunction|const|} A B) C) ({|Pure.imp|const|} A ({|Pure.imp|const|} B C))) := A : eta {|prop|type|} => B : eta {|prop|type|} => C : eta {|prop|type|} => {|Pure.equal_intr|axiom|} ({|Pure.imp|const|} ({|Pure.conjunction|const|} A B) C) ({|Pure.imp|const|} A ({|Pure.imp|const|} B C)) (H : eps ({|Pure.imp|const|} ({|Pure.conjunction|const|} A B) C) => Ha : eps A => Hb : eps B => H ({|Pure.conjunctionI|thm|} A B Ha Hb)) (H : eps ({|Pure.imp|const|} A ({|Pure.imp|const|} B C)) => Ha : eps ({|Pure.conjunction|const|} A B) => H ({|Pure.conjunctionD1|thm|} A B Ha) ({|Pure.conjunctionD2|thm|} A B Ha)).",
 //   ].join("\n");
 // }
-
-
-
