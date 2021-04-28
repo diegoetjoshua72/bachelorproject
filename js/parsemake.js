e822971f1268c5f42433319bf03cd9b7fbb1556f
diegoetjoshua72 <diegoetjoshua72@users.noreply.github.com>
2021-04-28 16:01:42 +0000

    deploy: dcf614c48ef357d016a9ed863e026cfc9d04daa7

--- js/parsemake.js
+++ js/parsemake.js
@@ -4,26 +4,74 @@ let example = `%.dko:
 %.koo:
 	$(MAKE) --silent -f deps.mk -f kontroli.mk $*.dko | xargs cat | kocheck $(KOFLAGS) -`
 
+ 
+
+function fetch_make_text_from_url(url){
+    const url = document.getElementById("urlmake").value;
+    if (url != "") {
+        fetch(url)
+            .then(check_fetch)
+            .then((result) => {
+                result
+                    .text() //if the string is 404 not found
+                    .then((string) => {
+                        console.log(
+                            "THIS IS THE STRING WE GET FROM THE URL :: ",
+                            string
+                        );
+                        split_tokens(string);
+                    })
+                    .catch((err) => {
+                        console.log("ERROR :", err);
+                        display_error_dom(err, context_id);
+                    });
+            })
+            .catch((err) => {
+                console.log("ERROR :", err);
+                // display_error_dom(err, context_id);
+            });
+    } else {
+        display_error_dom("Empty url field", context_id);
+    }
+}
+
 function split_tokens(text){
     let result = text.trim();
     result = text.split(" ");
     console.log("FILES: ",text.match(/\w+.mk/g));
     console.log("NAMES: ", text.match(/\w+:/g));
+    files = text.match(/\w+:/g);
     console.log(result);
+    return files;
 }
 
+
+//i need to have the link to the initial .mk file 
 //\w+.mk
 // two regular expressions one for the names that end in .ml
-split_tokens(example);
+console.log(fetch_make_text_from_url("https://raw.githubusercontent.com/diegoetjoshua72/bachelorproject/master/examples/kontroli.mk"))
 
 //anytime there is a \n\t or a space its a new word 
 //would be nice to match words with any number of spaces in between
 // text.match(/[a-z'\-]+/gi);
 
 function generate_gitraw_urls(filenames){
+
     return 0;
 }
 
 function fetch_files_data(urls){
-
+    return 0;
 }
+
+function generate_html_fs(){
+    return 0;
+}
+
+
+
+//TODO
+//will do that after this example works tho
+//but i also need to be able to handle relative paths so ../ should make it go one folder down when making the github urls 
+
+//TODO when i do run and i have multiple files they should also be passed in some order 
