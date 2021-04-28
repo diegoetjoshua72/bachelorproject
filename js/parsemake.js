b4f68cc08b4d92b57aab5448b29e42c7a1bb2b80
diegoetjoshua72 <diegoetjoshua72@users.noreply.github.com>
2021-04-28 15:08:29 +0000

    deploy: 405a417e487460a4301d6415bac439586b5d7418

--- js/parsemake.js
+++ js/parsemake.js
@@ -7,12 +7,15 @@ let example = `%.dko:
 function split_tokens(text){
     let result = text.trim();
     result = text.split(" ");
+    console.log("FILES: ",text.match(/\w+.mk/));
+    console.log("NAMES: ", text.match(/\w+:/));
     console.log(result);
-
 }
 
+//\w+.mk
+// two regular expressions one for the names that end in .ml
 split_tokens(example);
 
-
+//anytime there is a \n\t or a space its a new word 
 //would be nice to match words with any number of spaces in between
 // text.match(/[a-z'\-]+/gi);
