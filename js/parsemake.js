4bfaa0277cc4cca70fbb69f513ee7cf0fe0ffcbf
diegoetjoshua72 <diegoetjoshua72@users.noreply.github.com>
2021-04-28 15:20:57 +0000

    deploy: dec108fe8edd47fd834dec4b4966096d36de6e2b

--- js/parsemake.js
+++ js/parsemake.js
@@ -7,8 +7,8 @@ let example = `%.dko:
 function split_tokens(text){
     let result = text.trim();
     result = text.split(" ");
-    console.log("FILES: ",text.match(/\w+.mk/));
-    console.log("NAMES: ", text.match(/\w+:/));
+    console.log("FILES: ",text.match(/\w+.mk/g));
+    console.log("NAMES: ", text.match(/\w+:/g));
     console.log(result);
 }
 
@@ -19,3 +19,11 @@ split_tokens(example);
 //anytime there is a \n\t or a space its a new word 
 //would be nice to match words with any number of spaces in between
 // text.match(/[a-z'\-]+/gi);
+
+function generate_gitraw_urls(filenames){
+    return 0;
+}
+
+function fetch_files_data(urls){
+
+}
