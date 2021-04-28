c903f75d2b02a9e8fe148c99a1d7fdd5f96ca0d0
diegoetjoshua72 <diegoetjoshua72@users.noreply.github.com>
2021-04-28 16:31:35 +0000

    deploy: bbd6e431535890cef71fa0d815a37dc1467d280b

--- js/editor.js
+++ js/editor.js
@@ -1,16 +1,16 @@
 
-export {Test, name}
+// export {Test, name}
 
-class Test{
-    static teststring = "this is some string that i would just like to access in rust"[Symbol.iterator]();
-    static get_test(){
-        return this.teststring.next().value;
-    }
-}
+// class Test{
+//     static teststring = "this is some string that i would just like to access in rust"[Symbol.iterator]();
+//     static get_test(){
+//         return this.teststring.next().value;
+//     }
+// }
 
-function name() {
-    return 'Rust';
-}
+// function name() {
+//     return 'Rust';
+// }
 
 
 var editor = 0;
