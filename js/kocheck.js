30bddaa210685f75e9bcb6a5786f55f2f8556271
diegoetjoshua72 <diegoetjoshua72@users.noreply.github.com>
2021-05-03 15:02:43 +0000

    deploy: d35b8faceb91f4df9d8bc09ebea8c1543caae776

--- js/kocheck.js
+++ js/kocheck.js
@@ -275,7 +275,9 @@ test.onclick = () => {
 // https://github.com/01mf02/kontroli-rs/blob/master/examples/sudoku/deps.mk
 function fetch_make_text_from_url(){
     // const url = "https://raw.githubusercontent.com//bachelorproject/master/examples/kontroli.mk";
+    remove_all_errors_dom();
     const url = document.getElementById("urlmake").value; //continue here
+    console.log(url);
     if (url != "") {
         fetch(url)
             .then(check_fetch)
