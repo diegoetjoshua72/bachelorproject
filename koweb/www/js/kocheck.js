9372055fc364eece31eb1c02977c364285c6a611
kami0xff <chopin13760@gmail.com>
2021-05-12 10:53:28 +0200

    working on fetching in wasm

--- koweb/www/js/kocheck.js
+++ koweb/www/js/kocheck.js
@@ -279,7 +279,7 @@ test_click.onclick = () => {
     // };
     // xhr.send();
     
-    console.log("RANGE : ", window.editor.getValueInRange({startLineNumber: 1, endLineNumber: 3}))
+    // console.log("RANGE : ", window.editor.getValueInRange({startLineNumber: 1, endLineNumber: 3}))
     console.log("EDITOR : ", window.moncaco.editor);
     console.log("VALUE MON : ", window.monaco.editor.getValue());
     console.log("RANGE MON : ", window.monaco.editor.getValueInRange({startLineNumber: 1, endLineNumber: 3}));
