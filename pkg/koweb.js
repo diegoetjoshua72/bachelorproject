2c1d7a56813eda81ff5609b233f88486d7bbd07c
diegoetjoshua72 <diegoetjoshua72@users.noreply.github.com>
2021-05-18 11:16:34 +0000

    deploy: 5f1ccb54aa0f1af89ee687cea2ec31294ceee898

--- pkg/koweb.js
+++ pkg/koweb.js
@@ -608,7 +608,7 @@ async function init(input) {
         var ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper290 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper294 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 137, __wbg_adapter_28);
         return addHeapObject(ret);
     };
