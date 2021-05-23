db521620adfa211461cc4852cb067ef1afec342c
diegoetjoshua72 <diegoetjoshua72@users.noreply.github.com>
2021-05-23 06:48:00 +0000

    deploy: 312a8d74797b41b19bc79c323f971861e724b26a

--- pkg/koweb.js
+++ pkg/koweb.js
@@ -514,7 +514,7 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper162 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper164 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 66, __wbg_adapter_26);
         return addHeapObject(ret);
     };
